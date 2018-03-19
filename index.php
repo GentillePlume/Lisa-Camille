7272d2fe5cdafac3d12c8b76c2eaa40e23992585
Gentille Plume <melvin.lemoine@feather-project.org>
2018-03-19 16:49:06 +0100

    Developed the initial index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lisa Camille</title>
+        
+        <meta charset="utf-8">
+        
+        <!--########## STYLES SHEETS ##########-->
+        
+        <!--NAVIGATION BAR-->
+        <link rel="stylesheet" type="text/css" href="styles/navbar/desktop.css">
+        
+        <!--HEADER-->
+        <link rel="stylesheet" type="text/css" href="styles/header/desktop.css">
+        
+        <!--PRESENTATION-->
+        <link rel="stylesheet" type="text/css" href="styles/presentation/desktop.css">
+    </head>
+
+<!--####################################################################################################-->
+    
+    <body>
+    
+        <!--NAVIGATION BAR-->
+        <? include 'elements/navbar.html'?>
+        
+        <!--HEADER-->
+        <? include 'elements/header.html'?>
+        
+        <!--PRESENTATION-->
+        <? include 'elements/presentation.html'?>
+    </body>
+    
+</html>
