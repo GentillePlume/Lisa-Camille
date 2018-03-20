4293d8fe72fd4ec28b17349b2ff5d45c75f3ea89
Gentille Plume <melvin.lemoine@feather-project.org>
2018-03-20 17:20:34 +0100

    Created all needed files for the particles background work

---
+++ scripts/typedtext.js
@@ -0,0 +1,18 @@
+var typed = new Typed('header div div p', {
+    strings: ["Artiste, ^1000 youtubeuse, ^1000 compositrice, ^1000 et interprète"],
+    typeSpeed: 60,
+    loop: false,
+    backSpeed: 15,
+    backDelay: 3000,
+    showCursor: false,
+    startDelay: 4000,
+});
+
+var typed = new Typed('header div div h1', {
+    strings: ["Lisa Camille"],
+    typeSpeed: 150,
+    loop: false,
+    backSpeed: 15,
+    backDelay: 3000,
+    showCursor: false,
+});
