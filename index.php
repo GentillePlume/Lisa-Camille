8e619e44c96d48dc6729b07fdbb694c443665f0f
Gentille Plume <melvin.lemoine@feather-project.org>
2018-03-20 17:24:29 +0100

    Added required files for the music section

--- index.php
+++ index.php
@@ -23,6 +23,9 @@
         
         <!--PRESENTATION-->
         <link rel="stylesheet" type="text/css" href="styles/presentation/desktop.css">
+        
+        <!--MUSIC-->
+        <link rel="stylesheet" type="text/css" href="styles/music/desktop.css">
     </head>
 
 <!--####################################################################################################-->
@@ -39,6 +42,9 @@
         
         <!--PRESENTATION-->
         <?php include 'elements/presentation.html'?>
+        
+        <!--MUSIC-->
+        <?php include 'elements/music.html'?>
         
         <!--########## SCRIPTS ##########-->
         
