66fe2576472cab474ba80ad1e43c95771232a943
Gentille Plume <melvin.lemoine@feather-project.org>
2018-04-20 22:44:48 +0200

    Removed a useless script

--- scripts/app.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'scripts/particlesjs-config.json', function() {
-  console.log('particles.js config loaded');
-});
