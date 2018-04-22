2383777465965e132cc3f352b2c2a2bfebd87513
Gentille Plume <melvin.lemoine@feather-project.org>
2018-04-22 11:14:25 +0200

    Removed all useless things for the next developement version + minor corrections

--- index.php
+++ index.php
@@ -1,77 +1,40 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Lisa Camille</title>
-        
-        <meta charset="utf-8">
-        
-        <!--########## STYLES SHEETS ##########-->
-        
-        <!--TOOLS & RESSOURCES-->
-        <link href="resources/animate.css" rel="stylesheet">
-        <link href="resources/font-awesome/web-fonts-with-css/css/fontawesome-all.css" rel="stylesheet">
-        <link href="resources/fonts/fonts.css" rel="stylesheet">
-        
-        <!--GENERAL PROPERTIES-->
-        <link href="styles/general.css" rel="stylesheet">
-        
-        <!--NAVIGATION BAR-->
-        <link rel="stylesheet" type="text/css" href="styles/navbar/desktop.css">
-        
-        <!--HEADER-->
-        <link rel="stylesheet" type="text/css" href="styles/header/desktop.css">
-        
-        <!--PRESENTATION-->
-        <link rel="stylesheet" type="text/css" href="styles/presentation/desktop.css">
-        
-        <!--QUOTES-->
-        <link rel="stylesheet" type="text/css" href="styles/quotes/desktop.css">
-        
-        <!--MUSIC-->
-        <link rel="stylesheet" type="text/css" href="styles/music/desktop.css">
-        
-        <!--FOOTER-->
-        <link rel="stylesheet" type="text/css" href="styles/footer/desktop.css">     
-        
-    </head>
+
+<head>
+    <title>Lisa Camille</title>
+
+    <meta charset="utf-8">
+
+    <!--########## STYLES SHEETS ##########-->
+
+    <!--TOOLS & RESSOURCES-->
+    <link href="resources/animate.css/animate.css" rel="stylesheet">
+    <link href="resources/font-awesome/web-fonts-with-css/css/fontawesome-all.css" rel="stylesheet">
+    <link href="resources/fonts/fonts.css" rel="stylesheet">
+
+    <!--MAIN STYLE SHEET-->
+    <link href="styles/styles.css" rel="stylesheet">
+
+</head>
 
 <!--####################################################################################################-->
+
+<body>
+
    
-    <body>
-    
-       <div id="overlay"></div>
-       
-        <!--########## ELEMENTS ##########-->
-        
-        <!--HEADER-->
-        <?php include 'elements/header.html'?>
-        
-        <!--PRESENTATION-->
-        <?php include 'elements/presentation.html'?>
-        
-        <!--QUOTE-->
-        <?php include 'elements/quotes.html'?>
-        
-        <!--MUSIC-->
-        <?php include 'elements/music.html'?>
-        
-        <!--FOOTER-->
-        <?php include 'elements/footer.html'?>
-        
-        <!--########## SCRIPTS ##########-->
-        
-        <!--RESOURCES-->
-        <script src="resources/jquery.js"></script>
-        <script src="https://code.jquery.com/jquery-migrate-3.0.1.js"></script>
-        <script src="resources/typed.js/lib/typed.js"></script>
-        
-        <!--OTHER SCRIPTS-->
-        <script src="scripts/navbar.js" type="text/javascript"></script>
-        <script src="scripts/typedtext.js" type="text/javascript"></script>    
-        <script src="scripts/quotes.js" type="text/javascript"></script>     
-        <script src="scripts/music.js" type="text/javascript"></script>
-        <script src="scripts/smoothscrolling.js" type="text/javascript"></script>
-              
-    </body>
-    
+   
+    <!--########## SCRIPTS ##########-->
+
+    <!--RESOURCES-->
+    <script src="resources/jquery.js"></script>
+    <script src="https://code.jquery.com/jquery-migrate-3.0.1.js"></script>
+    <script src="resources/typed.js/lib/typed.js"></script>
+
+    <!--OTHER SCRIPTS-->
+    <script src="scripts/typedtext.js" type="text/javascript"></script>
+    <script src="scripts/smoothscrolling.js" type="text/javascript"></script>
+
+</body>
+
 </html>
