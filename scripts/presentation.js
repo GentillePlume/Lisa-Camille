c45bcd6b0829050c50f7606d4991b3bb3cfcb973
Gentille Plume <melvin.lemoine@feather-project.org>
2018-04-23 22:28:52 +0200

    Developed the basic presentation section script (about button)(not finished)

---
+++ scripts/presentation.js
@@ -0,0 +1,60 @@
+var about = $('#about');
+var close = $('#closeabout');
+var panel = $('#presentation > div:first-child');
+var panelb = $('#presentation > div:first-child > div');
+
+about.click(function () {
+    console.log("about button clicked");
+
+    about[0].style.height = 100 + "%";
+    about[0].style.backgroundColor = "rgba(0, 108, 255, 1)";
+    document.querySelector('#presentation a > p').style.display = "none";
+    about[0].style.cursor = "default";
+
+    setTimeout(function () {
+        about[0].style.width = 100 + "%";
+    }, 500);
+
+    setTimeout(function () {
+        about[0].style.marginLeft = 25 + "%";
+        about[0].style.width = 75 + "%";
+    }, 1200);
+
+    setTimeout(function () {
+        close[0].style.display = "flex";
+        panel[0].style.backgroundImage = "url(https://i.imgur.com/r15KJmc.jpg)";
+        panel[0].style.backgroundRepeat = 'no-repeat';
+        panel[0].style.backgroundPosition = 'center';
+        panel[0].style.backgroundSize = 'cover';
+        panel[0].style.backgroundColor = 'white';
+    }, 2000);
+});
+
+
+close.click(function () {
+    setTimeout(function () {
+        close[0].style.width = 0;
+        $('#closeabout p')[0].style.display = "none";
+        about[0].style.marginLeft = 0;
+        about[0].style.width = 100 + "%";
+    }, 0);
+
+    setTimeout(function () {
+        about[0].style.width = 25 + "%";
+        
+        close[0].style.display = "none";
+        close[0].style.width = 25 + "%";
+        $('#closeabout p')[0].style.display = "inline-block";
+        
+    }, 1000);
+
+    setTimeout(function () {
+        about[0].style.height = 10 + "%";
+        about[0].style.backgroundColor = "rgba(0, 108, 255, 0.8)";
+        about[0].style.cursor = "pointer";
+    }, 1500);
+    
+    setTimeout(function () {
+        document.querySelector('#presentation a > p').style.display = "inline-block";
+    }, 2000);    
+});
